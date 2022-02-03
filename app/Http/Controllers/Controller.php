af2d66a2100eb2db4df70f7383f7965eb001164a
khaled-madani <khaled15120010@gmail.com>
2022-02-03 14:00:51 +0200

    thirdV

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    public function about2()
+    {
+        return 'about2 page';
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return 'Homepage';
 });
 
 
-Route::get('about',function(){
-    return 'about page';
+Route::get('news',function(){
+    return 'News Page';
+});
+
+Route::get('news/{num}',function($num){
+    return 'Singel News Page :' . $num;
+});
+
+
+Route::get('users/{name?}',function($name){
+    return "Welcome : $name";
 });
 
 
 
 
 
-Route::get('contact',function(){
-    return 'contact page';
-});
 
-Route::get('contact2',function(){
-    return 'contact page';
-});
+
+
+
+
+
+
+
+
+

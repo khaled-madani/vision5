f4d3d88c50bedd8dad7a6773d6469f8bf023ec24
khaled-madani <khaled15120010@gmail.com>
2022-02-04 21:04:02 +0200

    fourdV

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,54 @@ Route::get('news',function(){
     return 'News Page';
 });
 
+
+
+// Route::get('users/{name?}',function($name = 'khaled'){
+//     return "Welcome : $name";
+// });
 Route::get('news/{num}',function($num){
     return 'Singel News Page :' . $num;
-});
+})->whereNumber('num');   // helper method laravel  فقط يكون ارقام
 
 
 Route::get('users/{name?}',function($name){
     return "Welcome : $name";
+})->whereAlpha('name');  // تقبل فقط حروف
+
+
+Route::get('users2/{name?}',function($name){
+    return "Welcome : $name";
+})->whereAlphaNumeric('name');   //  تقبل فقط حروف و ارقام
+
+
+
+// Route::get('users/{name?}',function($name){
+//     return "Welcome : $name";
+// });
+
+
+Route::get('profile/{name?}',function($name3 = null){
+    return "Welcome : $name3";
 });
 
+// Route::get('admin/post',function(){
+//     return "Welcome admain";
+// });
 
+Route::prefix('admin')->group(function(){
+Route::get('/',function(){
+    return "Welcome admin";
+});
+Route::get('/posts',function(){
+    return 'admin Posts';
+});
+Route::get('/articles',function(){
+    return 'admin articles';
+});
+Route::get('/users  ',function(){
+    return 'admin users';
+})->name('admin_user');
+});
 
 
 
